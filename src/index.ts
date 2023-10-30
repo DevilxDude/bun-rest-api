d9a42ecf945aaeb72eed005bfd488f1ed297adb9
DevilxDude <ddude786@gmail.com>
2023-10-30 20:02:41 +0530

    Understanding how elysia uses global states and methods

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia()
+  .state({
+    version: 1,
+    id: 1,
+    email: "jane@gmail.com",
+  })
+  .decorate("getDate", () => Date.now())
   .get("/", () => "Hello Bun Dev, I am gonna build RESTFUL APIs")
   .get("/post/:id", ({ params: { id } }) => {
     // return `Hello ID: ${id}`;
     return { id, title: "Learn bun" };
   })
-  .post("/post", ({ body, set }) => {
+  .post("/post", ({ body, set, store }) => {
+    console.log({ store });
+
     set.status = 201;
     return body;
   })
@@ -15,7 +23,9 @@ const app = new Elysia()
       tracks: ["Dancing Feat", "Sam I", "Animals"],
     };
   })
-  .get("/tracks", () => {
+  .get("/tracks", ({ store, getDate }) => {
+    console.log({ store, date: getDate() });
+
     return new Response(
       JSON.stringify({
         tracks: ["Dancing Feat", "Sam I", "Animals"],
