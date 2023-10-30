78308ab4328fe49dd165d69a2866c2f9c33d733e
DevilxDude <ddude786@gmail.com>
2023-10-30 20:13:50 +0530

    Setting up basic version API group on Elysia

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,20 @@
 import { Elysia } from "elysia";
-import { plugin } from "./plugin";
 
-const app = new Elysia()
-  .state({
-    version: 1,
-    id: 1,
-    email: "jane@gmail.com",
-  })
-  .decorate("getDate", () => Date.now())
-  .use(plugin)
-  .get("/", () => "Hello Bun Dev, I am gonna build RESTFUL APIs")
-  .get("/post/:id", ({ params: { id } }) => {
-    // return `Hello ID: ${id}`;
-    return { id, title: "Learn bun" };
-  })
-  .post("/post", ({ body, set, store }) => {
-    console.log({ store });
+const app = new Elysia();
 
-    set.status = 201;
-    return body;
-  })
-  .get("/track/*", () => {
-    return {
-      tracks: ["Dancing Feat", "Sam I", "Animals"],
-    };
-  })
-  .get("/tracks", ({ store, getDate }) => {
-    console.log({ store, date: getDate() });
+app.group("/v1", (app) =>
+  app
+    .get("/", () => "Version 1")
+    .group("/products", (app) =>
+      app
+        .post("/", () => "Create Product")
+        .get("/:id", () => "Get Product by ID")
+        .put("/:id", () => "Update Product by ID")
+        .delete("/:id", () => "Delete Product by iD")
+    )
+);
 
-    console.log({ "plugin-version": store["plugin-version"] });
-
-    return new Response(
-      JSON.stringify({
-        tracks: ["Dancing Feat", "Sam I", "Animals"],
-      }),
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        status: 200,
-      }
-    );
-  })
-  .listen(3000);
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
