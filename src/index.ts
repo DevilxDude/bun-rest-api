07baa58d94e5c459b5c2fac362b65ac5851f9801
DevilxDude <ddude786@gmail.com>
2023-10-30 20:19:21 +0530

    Modularized routes to have unique instances for each models

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,29 @@ import { Elysia } from "elysia";
 
 const app = new Elysia();
 
+const user = new Elysia().group("/user", (app) =>
+  app
+    .post("/sign-in", ({ body }) => {
+      return body;
+    })
+    .post("/sign-up", () => "Signup route")
+    .post("/profile", () => "Profile ROuter")
+    .get("/:id", () => "Get User by id Route")
+);
+
+const products = new Elysia().group("/products", (app) =>
+  app
+    .post("/", () => "Create Product")
+    .get("/:id", () => "Get Product by ID")
+    .put("/:id", () => "Update Product by ID")
+    .delete("/:id", () => "Delete Product by iD")
+);
+
 app.group("/v1", (app) =>
   app
     .get("/", () => "Version 1")
-    .group("/products", (app) =>
-      app
-        .post("/", () => "Create Product")
-        .get("/:id", () => "Get Product by ID")
-        .put("/:id", () => "Update Product by ID")
-        .delete("/:id", () => "Delete Product by iD")
-    )
+    .use(user)
+    .use(products)
 );
 
 app.listen(3000);
